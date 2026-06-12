0b449976c68408d7969430ee9c0d9914728c2aab
alihanmaran22 <120220440+alihanmaran22@users.noreply.github.com>
2026-06-12 17:32:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-# Secrets'tan string olarak al
-creds_json = st.secrets["GCP_JSON"]
+# Secrets'ı al ve düzgünce yükle
+creds_dict = json.loads(st.secrets["GCP_JSON"])
 
-# String'i dict'e çevir
-creds_dict = json.loads(creds_json)
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # Dosyayı bağla
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1mybm2kJtDHGUw5sruKp4tpSaV2PwINkfoTrIhCI82rY/edit").sheet1
+sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1mybm2kJtDHGUw5sruKp4tpSaV2PwINKfoTrIhCI82rY/edit").sheet1
 
 st.title("💊 Nextpharma Kalıcı Kayıt")
 
